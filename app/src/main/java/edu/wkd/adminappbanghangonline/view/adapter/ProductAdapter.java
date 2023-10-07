35775177b248686b0868508f087070b0c92f889b
nguyen chi thuan <thuan0936a@gmail.com>
2023-10-07 17:48:08 +0700

    thuan 7/1- tao khung app admin

---
+++ app/src/main/java/edu/wkd/adminappbanghangonline/view/adapter/ProductAdapter.java
@@ -0,0 +1,4 @@
+package edu.wkd.adminappbanghangonline.view.adapter;
+
+public class ProductAdapter {
+}
---
+++ app/src/main/java/edu/wkd/adminappbanghangonline/view/fragment/NotificationFragment.java
@@ -0,0 +1,38 @@
+package edu.wkd.adminappbanghangonline.view.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.wkd.adminappbanghangonline.R;
+
+public class NotificationFragment extends Fragment {
+
+    public static NotificationFragment newInstance() {
+        NotificationFragment fragment = new NotificationFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notification, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
